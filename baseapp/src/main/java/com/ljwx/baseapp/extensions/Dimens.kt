92bef071a7dbc9ed1742c6f6e8c72a70e97a93d4
tianchaoming <tianchaoming@codemao.cn>
2025-02-05 18:03:03 +0800

    优化base app的依赖范围

--- baseapp/src/main/java/com/ljwx/baseapp/extensions/Dimens.kt
+++ baseapp/src/main/java/com/ljwx/baseapp/extensions/Dimens.kt
@@ -3,18 +3,9 @@ package com.ljwx.baseapp.extensions
 import android.content.Context
 import android.util.TypedValue
 import android.view.View
-import androidx.fragment.app.Fragment
 
 fun Context.dp2px(dp: Float): Float {
     return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics)
-}
-
-fun Fragment.dp2px(dp: Float): Float {
-    return TypedValue.applyDimension(
-        TypedValue.COMPLEX_UNIT_DIP,
-        dp,
-        requireContext().resources.displayMetrics
-    )
 }
 
 fun View.dp2px(dp: Float): Float {
